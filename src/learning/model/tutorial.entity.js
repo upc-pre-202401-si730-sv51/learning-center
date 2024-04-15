a619880c738437e177961a7c8c7fc6a6e1fd9412
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-15 11:17:31 -0500

    feat(tutorials): added tutorial entity.

---
+++ src/learning/model/tutorial.entity.js
@@ -0,0 +1,9 @@
+export class Tutorial {
+    constructor(id, title, description, published) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.published = published;
+    this.status = this.published === true ? "Published" : "Unpublished";
+  }
+}
