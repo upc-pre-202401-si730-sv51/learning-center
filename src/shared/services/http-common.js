9cbd5fdf7a2aeab2027105c92a0f3519ee7ef2e4
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-15 11:08:08 -0500

    feat(api-client): added default configuration for remote services client.

---
+++ src/shared/services/http-common.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+/**
+ * Create an Axios instance with a base URL and JSON content type.
+ * @type {axios.AxiosInstance}
+ */
+const http = axios.create({
+    baseURL: API_BASE_URL,
+    headers: { 'Content-type': 'application/json' }
+});
+
+export default http;
