9229b9eff952dedc3b55ac0412175eba0e0b6e50
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-16 18:23:15 -0500

    feat(main-view): improved title, icon and toolbar..

--- src/app.vue
+++ src/app.vue
@@ -26,6 +26,8 @@ export default {
       <template #start>
         <pv-button class="p-button-text text-white" icon="pi pi-bars" @click="toggleDrawer"/>
         <h3>ACME Learning Center</h3>
+      </template>
+      <template #end>
         <div class="flex-column">
           <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
             <pv-button :href="href" class="p-button-text text-white" @click="navigate">{{ item.label }}</pv-button>
