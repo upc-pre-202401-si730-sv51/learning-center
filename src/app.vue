a4834183eabad28db28daa960322f7fb3e4c925a
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-27 20:20:04 -0500

    feat(tutorials): implemented tutorials management with reusable components.

--- src/app.vue
+++ src/app.vue
@@ -7,6 +7,7 @@ export default {
       drawer: false,
       items: [
         { label: 'Home', to: '/home'},
+        { label: 'Tutorials', to: '/tutorials' },
         { label: 'About', to: '/about'}
       ]
     }
