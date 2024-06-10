df9932201dfbe12803d2d8d3539447dec1776805
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-10 11:23:20 -0500

    feat(iam): added and named authentication related routes.

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,28 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import TutorialManagementComponent from "../learning/pages/tutorial-management.component.vue";
+import SignInComponent from "../iam/pages/sign-in.component.vue";
+import SignUpComponent from "../iam/pages/sign-up.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/home',    component: HomeComponent},
+        { path: '/home', name: 'home',   component: HomeComponent},
         { path: '/tutorials', component: TutorialManagementComponent, meta: { title: 'Tutorials' } },
         { path: '/about',   component: AboutComponent},
+        { path: '/sign-in', name: 'sign-in', component: SignInComponent, meta: { title: 'Sign In' } },
+        { path: '/sign-up', name: 'sign-up', component: SignUpComponent, meta: { title: 'Sign Up' } },
         { path: '/',        redirect: '/home'}
     ]
+});
+
+/**
+ * Set Business name as prefix for each page title
+ */
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    next();
 });
 
 export default router;
